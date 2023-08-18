ae3d5c80163de3e62fa66fb4076e29aad81ceba0
Callan Goldeneye <callan@goldeneye.app>
2023-08-18 12:23:04 -0700

    update metadata

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import moment from "moment";
+import PropTypes from "prop-types";
 
 const defaultStyles = {
   clockStyle: {
-    height: '8rem',
+    height: "8rem",
     margin: 0,
     padding: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    fontSize: '61px',
-    fontFamily: 'sans-serif',
-    letterSpacing: '5px',
-    textShadow: '0 0 10px #fff',
-    textTransform: 'uppercase'
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    fontSize: "61px",
+    fontFamily: "sans-serif",
+    letterSpacing: "5px",
+    textShadow: "0 0 10px #fff",
+    textTransform: "uppercase",
   },
   clockHeaderStyle: {
-    margin: '13px',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    margin: "13px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   clockSubHeader: {
-    marginBottom: '10px',
-    fontSize: '13px',
-    letterSpacing: 'initial'
-  }
+    marginBottom: "10px",
+    fontSize: "13px",
+    letterSpacing: "initial",
+  },
 };
 
 class ReactClock extends Component {
-
   static propTypes = {
     endDate: PropTypes.string,
     color: PropTypes.string,
@@ -40,11 +39,11 @@ class ReactClock extends Component {
     clockShadow: PropTypes.string,
     day: PropTypes.bool,
     clockDigitStyle: PropTypes.string,
-    clockSeparator: PropTypes.string
+    clockSeparator: PropTypes.string,
   };
 
   state = {
-    timeRemaining: moment(this.props.endDate).diff(Date.now())
+    timeRemaining: moment(this.props.endDate).diff(Date.now()),
   };
 
   componentDidMount() {
@@ -56,46 +55,55 @@ class ReactClock extends Component {
   }
 
   tick = () => {
-    this.setState({ timeRemaining: moment(this.props.endDate).diff(Date.now()) });
-  }
+    this.setState({
+      timeRemaining: moment(this.props.endDate).diff(Date.now()),
+    });
+  };
 
   render() {
     const { day, style, clockSeparator } = this.props;
     const duration = moment.duration(this.state.timeRemaining);
-    const daysLeft = String(duration.days()).padStart(2, '0');
+    const daysLeft = String(duration.days()).padStart(2, "0");
     const isDayEnabled = day === undefined ? false : day;
     return (
       <div
         style={{
           ...defaultStyles.clockStyle,
-          ...style
-        }}>
-        {isDayEnabled && <div style={{ ...defaultStyles.clockHeaderStyle }}>
-          <div style={{ ...defaultStyles.clockSubHeader }}>DAYS</div>
-          <div>{daysLeft}</div>
-        </div>}
-        {isDayEnabled && <ReactClock.Seperator clockSeparator={clockSeparator} />}
+          ...style,
+        }}
+      >
+        {isDayEnabled && (
+          <div style={{ ...defaultStyles.clockHeaderStyle }}>
+            <div style={{ ...defaultStyles.clockSubHeader }}>DAYS</div>
+            <div>{daysLeft}</div>
+          </div>
+        )}
+        {isDayEnabled && (
+          <ReactClock.Seperator clockSeparator={clockSeparator} />
+        )}
         <div style={{ ...defaultStyles.clockHeaderStyle }}>
           <div style={{ ...defaultStyles.clockSubHeader }}>HOURS</div>
-          <div>{String(duration.hours()).padStart(2, '0')}</div>
+          <div>{String(duration.hours()).padStart(2, "0")}</div>
         </div>
         <ReactClock.Seperator clockSeparator={clockSeparator} />
         <div style={{ ...defaultStyles.clockHeaderStyle }}>
           <div style={{ ...defaultStyles.clockSubHeader }}>MINUTES</div>
-          <div>{String(duration.minutes()).padStart(2, '0')}</div>
+          <div>{String(duration.minutes()).padStart(2, "0")}</div>
         </div>
         <ReactClock.Seperator clockSeparator={clockSeparator} />
         <div style={{ ...defaultStyles.clockHeaderStyle }}>
           <div style={{ ...defaultStyles.clockSubHeader }}>SECONDS</div>
-          <div>{String(duration.seconds()).padStart(2, '0')}</div>
+          <div>{String(duration.seconds()).padStart(2, "0")}</div>
         </div>
       </div>
     );
   }
 
   static Seperator = ({ clockSeparator, shouldShow = true }) => {
-    return <span>{shouldShow ? (clockSeparator ? clockSeparator : '.') : ''}</span>;
-  }
+    return (
+      <span>{shouldShow ? (clockSeparator ? clockSeparator : ".") : ""}</span>
+    );
+  };
 }
 
 export default ReactClock;
